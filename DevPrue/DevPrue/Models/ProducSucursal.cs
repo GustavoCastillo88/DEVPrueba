9b9480c6bfa6be8add0a304041f514233a1b19e0
GustavoCastillo88 <gustavoavc86@gmail.com>
2022-06-27 02:46:09 -0500

    Se agrega el Proyecto

---
+++ DevPrue/DevPrue/Models/ProducSucursal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DevPrue.Models
+{
+    public class ProducSucursal
+    {
+        public int id { get; set; }
+        public string nombreProducto { get; set; }
+        public int codigoDeBarra { get; set; }
+        public int cantidad { get; set; }
+        public int precio { get; set; }
+        public string nombre { get; set; }
+
+    }
+}
